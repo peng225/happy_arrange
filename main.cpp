12d35b01064b937a2ae3431a94b4f69d1149b956
Shinya Hayashi <shinyaUT0925@gmail.com>
2014-08-17 13:51:41 +0900

    Changed the way to the input file and its contents.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
 #include <assert.h>
 #include <stdlib.h>
-#include <iostream>
+#include <string.h>
 #include <stdlib.h>
+
+#include <iostream>
+#include <string>
 #include <vector>
 #include <list>
 #include <map>
 #include <limits>
+#include <fstream>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
+
 #include "node.h"
+
+// typedef boost::char_separator<char> char_separator;
+// typedef boost::tokenizer<char_separator> tokenizer;
 
 using std::cout;
 using std::cerr;
 using std::endl;
-using std::cin;
 using std::list;
 using std::map;
 using std::pair;
 using std::begin;
 using std::end;
+using std::string;
+using std::stoi;
+using std::ifstream;
 
 void removeInferiorNode(list<Node> &q, const multiset<int> &depts)
 {
@@ -96,67 +109,133 @@ void showVector(const vector<int> &v)
   }
 }
 
-
-int main(int argc, char** args)
+void defaultSettings(int &tmp_NUM_CHOICES, vector<int> &scores)
 {
-  //コマンドライン引数の数をチェック  
+  tmp_NUM_CHOICES = 3;
+  scores.resize(tmp_NUM_CHOICES + 1);
+  scores[0] = 5;
+  scores[1] = 3;
+  scores[2] = 2;
+  scores[3] = 0;
+}
+
+
+int main(int argc, char** argv)
+{
+  //コマンドライン引数のチェック  
   // 志望数と各志望度のスコアをセット
+  
   int tmp_NUM_CHOICES;
   vector<int> scores;
-  if(argc != 1){
-    tmp_NUM_CHOICES = atoi(args[1]) + 1;
-    if(argc != tmp_NUM_CHOICES + 2){
-      cerr << "Error: Wrong number of point arguments." << endl;
-      return 1;
-    }
 
-    scores.resize(tmp_NUM_CHOICES);
+  defaultSettings(tmp_NUM_CHOICES, scores);  
   
-    for(int i = 0; i < (int)scores.size(); i++){
-      scores[i] = atoi(args[i + 2]);
-      if(scores[i] < 0){
-	cerr << "A score must be a positive number." << endl;
-	return 1;
-      }
-    }
+  int opt;
+  char inputFileName[100];
+  while((opt = getopt(argc, argv, "hi:s:"))!=-1){
+    switch(opt){
+      /* 値をとらないオプション */
+    case 'h':    
+      /* getoptの返り値は見付けたオプションである. */
+      cout << "Have a nice day!" << endl;
+      exit(0);
+      
+      /* 値を取る引数の場合は外部変数optargにその値を格納する. */
+    case 'i':
+      // 入力ファイル名の設定
+      strcpy(inputFileName, optarg);
+      break;
+    // case 'n':
+    //   // 志望を出せる数を設定
+    //   tmp_NUM_CHOICES = atoi(optarg);
+    //   if(tmp_NUM_CHOICES <= 0){
+    // 	cerr << "The value of -n option must be positive." << endl;
+    // 	exit(1);
+    //   }
+    //   break;
+    case 's':
+      // 各志望のスコアを設定 
+      {	
+	string delim(":");
+        list<string> list_string;
+	scores.resize(0);
+	boost::split(list_string, optarg, boost::is_any_of(delim));
+	BOOST_FOREACH(string s, list_string) {
+	  scores.push_back(stoi(s));	  
+        }
 
-    for(int i = 0; i < (int)scores.size() - 1; i++){
-      if(scores[i] < scores[i + 1]){
-	cerr << "Wrong order between scores." << endl;
-	return 1;
+	// スコアがすべて正であるかチェック
+	BOOST_FOREACH(int num, scores){
+	  if(num <= 0){
+	    cerr << "Scores must be a positive number." << endl;
+	    return 1;
+	  }
+	}
+
+	// スコアの間の順序が正しいかチェック
+	for(int i = 0; i < (int)scores.size() - 1; i++){
+	  if(scores[i] < scores[i + 1]){
+	    cerr << "Wrong order between scores." << endl;
+	    return 1;
+	  }
+	}
+
+	tmp_NUM_CHOICES = scores.size();
+
+	// 希望から外れた場合のスコアは問答無用で0
+	scores.push_back(0);
+	
+	break;
       }
+      /* 以下二つのcaseは意味がないようだ.
+	 getoptが直接エラーを出力してくれるから.
+	 プログラムを終了するなら意味があるかも知れない */
+    case ':':
+      /* 値を取る引数に値がなかった場合:を返す. */
+      exit(1);
+      
+      /* getoptの引数で指定されなかったオプションを受け取ると?を返す. */
+    case '?':
+      exit(1);
     }
-  }else{
-    tmp_NUM_CHOICES = 3;
-    scores.resize(tmp_NUM_CHOICES + 1);
-    scores[0] = 5;
-    scores[1] = 3;
-    scores[2] = 2;
-    scores[3] = 0;
+  }  
+
+  const int NUM_CHOICES = tmp_NUM_CHOICES;    
+
+  // 入力ファイルを開く
+  std::ifstream ifs(inputFileName);  
+  if(ifs.fail()){
+    std::cerr << "Failed to open the file." << std::endl;
+    exit(1);
   }
 
-  const int NUM_CHOICES = tmp_NUM_CHOICES;
-  
+  string tstr;
+
   // 人数を読み込む
   int tmp_NUM_PEOPLE;
-  cin >> tmp_NUM_PEOPLE;
+  getline(ifs, tstr);
+  tmp_NUM_PEOPLE = stoi(tstr);  
   const int NUM_PEOPLE = tmp_NUM_PEOPLE;
   
   // 部署数を読み込む
   int tmp_NUM_DEPT;
-  cin >> tmp_NUM_DEPT;
+  getline(ifs, tstr);
+  tmp_NUM_DEPT = stoi(tstr);
   const int NUM_DEPT = tmp_NUM_DEPT;
+  
   cout << tmp_NUM_PEOPLE << ", " << tmp_NUM_DEPT << endl;
 
 
+  // 各部署の定員を読み込む
   vector<int> capacity(NUM_DEPT);   
-  for(vector<int>::iterator i = begin(capacity); i != end(capacity); i++){
-    // 各部署の定員を読み込む
-    cin >> *i;
+  for(vector<int>::iterator i = begin(capacity); i != end(capacity); i++){    
+    getline(ifs, tstr);
+    *i = stoi(tstr);
   }
   cout << "capacity:" << endl;
   showVector(capacity);
 
+  // 志望度ベクトルの定義と初期化
   vector<vector<int> > choices(NUM_PEOPLE);
   vector<int> choicesID(NUM_PEOPLE);
 
@@ -169,14 +248,23 @@ int main(int argc, char** args)
     choicesID.at(i) = i;
   }
 
-  // for(int i = 0; i < (int)choices.size(); i++){
+  // 志望度ベクトルの値を読み込む
   for(vector<vector<int> >::iterator i = begin(choices);
-      i != end(choices); i++){
-    for(int j = 0; j < NUM_CHOICES; j++){
+      i != end(choices); i++){    
+    string delim(" ");  
+    list<string> list_string;
+    getline(ifs, tstr);        
+    boost::split(list_string, tstr, boost::is_any_of(delim));
+    if((int)list_string.size() != NUM_CHOICES){
+      cerr << "Error : The size of score list is wrong." << endl;
+      exit(1);
+    }
+    
+    for(list<string>::iterator j = begin(list_string);
+	j != end(list_string); j++){
       // 各人のj番目の志望を読み込む
-      int dept;
-      cin >> dept;
-      i->at(dept) = scores.at(j);
+      int dept = stoi(*j);
+      i->at(dept) = scores.at(distance(begin(list_string), j));
     }
   }
 
